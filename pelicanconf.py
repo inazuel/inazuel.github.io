1b73480ee4866e83fa768e3a63adf95534ffb0b0
inazuel <inazuel@gmail.com>
2025-07-06 19:17:47 +0900

    pelicanconf.py 간결화

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,86 +1,61 @@
+import os
+
+# --- 기본 설정 ---
 AUTHOR = 'inazuel'
 SITENAME = 'record of my life'
 SITEURL = "https://inazuel.github.io"
-
 PATH = "content"
-
 TIMEZONE = 'Etc/GMT+9'
-
 DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 5 # 한 페이지당 게시물 수
 
-# Feed generation is usually not desired when developing
+# --- 피드 설정 (개발 시 보통 비활성화) ---
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
+# --- 링크 및 소셜 위젯 ---
 LINKS = (
     ("Pelican", "https://getpelican.com/"),
     ("Python.org", "https://www.python.org/"),
     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
     ("You can modify those links in your config file", "#"),
 )
-
-# Social widget
 SOCIAL = (
     ("You can add links in your config file", "#"),
     ("Another social link", "#"),
 )
 
-DEFAULT_PAGINATION = 1
+# --- 경로 설정 ---
+# pelicanconf.py 파일이 있는 디렉토리의 절대 경로
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+# 테마 경로 (pelicanconf.py 기준 themes/pelican-bootstrap3)
+THEME = os.path.join(BASE_DIR, 'pelican-themes', 'pelican-bootstrap3') # 'pelican-themes' 폴더가 BASE_DIR 안에 있을 때
 
-# 20250701 추가항목
-# path-specific metadata
+# 플러그인 경로 (pelicanconf.py 기준 pelican-plugins)
+PLUGIN_PATHS = [os.path.join(BASE_DIR, 'pelican-plugins')]
+PLUGINS = ['i18n_subsites']
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+
+# --- 정적 파일 및 메타데이터 설정 ---
+# images, extra 폴더, static 폴더의 내용을 output으로 복사
+STATIC_PATHS = ['images', 'extra', 'static']
+
+# 특정 파일에 대한 경로 메타데이터 (robots.txt 및 폰트 파일)
 EXTRA_PATH_METADATA = {
     "extra/robots.txt": {"path": "robots.txt"},
-}
-
-# static paths will be copied without parsing their contents
-STATIC_PATHS = [
-    "images",
-    "extra/robots.txt",
-]
-
-
-import os
-
-# pelicanconf.py 파일이 있는 디렉토리 (즉, C:\git_blog\blog\inazuel.github.io\)의 절대 경로
-PATH = os.path.abspath(os.path.dirname(__file__))
-
-# THEME 변수 설정:
-# PATH (현재 디렉토리) 안에 'pelican-themes' 폴더가 있고,
-# 그 안에 'pelican-bootstrap3' 폴더가 있다는 것을 명시합니다.
-THEME = os.path.join(PATH, 'pelican-themes', 'pelican-bootstrap3')
-
-# 플러그인 경로도 동일한 방식으로 설정 (만약 pelican-plugins도 inazuel.github.io 안에 있다면)
-PLUGIN_PATHS = [os.path.join(PATH, 'pelican-plugins')]
-PLUGINS = ['i18n_subsites'] # i18n_subsites 플러그인 활성화
-JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']} # Jinja2 i18n 확장 활성화
-
-# 게시물 상단에 저자 정보를 표시할지 여부
-SHOW_ARTICLE_AUTHOR = True
-
-# 게시물 상단에 카테고리 정보를 표시할지 여부
-SHOW_ARTICLE_CATEGORY = True
-
-# 메인 페이지에 글 목록 대신 글의 전체 내용을 표시할지 여부
-SHOW_FULL_ARTICLE = True
-
-
-# 기존 STATIC_PATHS에 'static' 추가
-STATIC_PATHS = ['images', 'static'] # 'static'이 이미 있다면 추가하지 않아도 됩니다.
-
-# 기존 STATIC_PATHS에 'static' 추가
-STATIC_PATHS = ['images', 'static'] # 'static'이 이미 있다면 추가하지 않아도 됩니다.
-
-# 폰트 파일 경로 지정
-EXTRA_PATH_METADATA = {
     'static/fonts/D2Coding-Ver1.3.2-20180524-all.ttf': {'path': 'static/fonts/D2Coding-Ver1.3.2-20180524-all.ttf'},
 }
-# 사용자 정의 CSS 파일 경로 (폰트 적용)
+
+# 사용자 정의 CSS 파일 경로
 CUSTOM_CSS = 'static/css/custom.css'
+
+# --- 테마 관련 표시 설정 ---
+SHOW_ARTICLE_AUTHOR = True     # 게시물 상단에 저자 정보 표시
+SHOW_ARTICLE_CATEGORY = True   # 게시물 상단에 카테고리 정보 표시
+SHOW_FULL_ARTICLE = True       # 메인 페이지에 글의 전체 내용 표시 (아니면 요약)
+
+# RELATIVE_URLS = True # 개발 시 문서 상대 URL 사용 (필요시 주석 해제)
